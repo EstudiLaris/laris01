dc2e883b7156b3ad1124fb2017bf7ff08671a626
jcunillf23 <jcunillf23.cursos@gmail.com>
2020-03-15 10:51:31 +0100

    logo nou

--- navBar.js
+++ navBar.js
@@ -8,13 +8,17 @@ function menuScroll() {
     var idLogo = document.getElementById("Logo");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "clnavBar" + " j-w3-animate-top";
-        idLogo.className = "img-height" + " j-w3-animate-top";
-        document.getElementById("subTitol").style.display = "none";
-        document.getElementById("titolPrincipal").style.width = "280px"
+        document.getElementById("titolMenu").style.display = "block";
+        /* idLogo.className = "img-height" + " j-w3-animate-top";*/
+        idLogo.style.display = "none";
+        /* document.getElementById("subTitol").style.display = "none";*/
+        /* document.getElementById("titolPrincipal").style.width = "280px"*/
     } else {
         navbar.className = navbar.className.replace(" j-w3-animate-top", "");
+        document.getElementById("titolMenu").style.display = "none";
+        idLogo.style.display = "block";
         idLogo.className = idLogo.className.replace("img-height j-w3-animate-top", "");
-        document.getElementById("subTitol").style.display = "block";
-        document.getElementById("titolPrincipal").style.width = "320px"
+
+        /* document.getElementById("titolPrincipal").style.width = "320px"*/
     }
 }
