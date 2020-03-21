1db059d5e7b6347f0930bfd4254d54a97d6e6865
jcunillf23 <jcunillf23.cursos@gmail.com>
2020-03-21 12:10:02 +0100

    Equip acabat amb modal

---
+++ modal.js
@@ -0,0 +1,9 @@
+function obrirModal(nomModal) {
+    var modal = document.getElementById(nomModal);
+    modal.style.display = "block";
+}
+
+function tancarModal(nomModal) {
+    var modal = document.getElementById(nomModal);
+    modal.style.display = "none";
+}
