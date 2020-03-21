70645c23a5f8974c82cc1be2fa85bc005c4ebd3f
jcunillf23 <jcunillf23.cursos@gmail.com>
2020-03-21 16:04:54 +0100

    prova show portfolio

--- portfolio.js
+++ portfolio.js
@@ -3,10 +3,12 @@ filterSelection("all")
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("column");
+
     if (c == "all") c = "";
     for (i = 0; i < x.length; i++) {
         w3RemoveClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+
     }
 }
 
