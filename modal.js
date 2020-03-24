27f6d9c3dc3f8d973cd8801258052052884cf409
jcunillf23 <jcunillf23.cursos@gmail.com>
2020-03-24 21:41:18 +0100

    modelOnLoad

--- modal.js
+++ modal.js
@@ -7,3 +7,13 @@ function tancarModal(nomModal) {
     var modal = document.getElementById(nomModal);
     modal.style.display = "none";
 }
+
+function obrirOnLoad(nomModal) {
+    var i = 0;
+    var modal = document.getElementById(nomModal);
+    modal.style.display = "block";
+    setTimeout(function() {
+        var modal = document.getElementById(nomModal);
+        modal.style.display = "none";
+    }, 3000);
+}
